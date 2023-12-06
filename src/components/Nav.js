96560021dcb4e0f73602114edb3c9a0ef13146a6
Sawyer <sawyer.pearson92@gmail.com>
2023-12-06 15:10:44 -0700

    nav styled and made responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,52 @@
 import '../styles/Nav.css';
 import {NavLink} from 'react-router-dom';
+import {RxDropdownMenu} from 'react-icons/rx';
+import {IoMdCloseCircleOutline} from 'react-icons/io';
+import {IconContext} from 'react-icons';
+import {useState} from 'react';
 
 function Nav() {
+
+    const [isNavVisible, setNavVisibility] = useState(true)
+    const toggleNavVisibility = () => {
+        setNavVisibility(!isNavVisible);
+    }
+
+
   return (
  <nav className='nav-main'>
     <ul className='nav-container'>
-        <li className='nav-item'>
+        {isNavVisible ? (<>
+            <li className='nav-item'>
             <NavLink to="/" className='nav-button'>Home</NavLink>
-        </li>
-        <li className='nav-item'>
-            <NavLink to="projects" className='nav-button'>Projects</NavLink>
-        </li>
-        <li className='nav-item'>
-            <NavLink to="skills" className='nav-button'>Skills</NavLink>
-        </li>
-        <li className='nav-item'>
-            <NavLink to="resume" className='nav-button'>Resume</NavLink>
-        </li>
-        <li className='nav-item'>
-            <NavLink to="contact" className='nav-button'>Contact</NavLink>
-        </li>
+            </li>
+            <li className='nav-item'>
+                <NavLink to="projects" className='nav-button'>Projects</NavLink>
+            </li>
+            <li className='nav-item'>
+                <NavLink to="skills" className='nav-button'>Skills</NavLink>
+            </li>
+            <li className='nav-item'>
+                <NavLink to="resume" className='nav-button'>Resume</NavLink>
+            </li>
+            <li className='nav-item'>
+                <NavLink to="contact" className='nav-button'>Contact</NavLink>
+            </li>
+            <IconContext.Provider value={{color: "white", size: "2em", className: "close-menu-icon"}}>
+                <IoMdCloseCircleOutline onClick={toggleNavVisibility}/>
+            </IconContext.Provider>
+        </>
+        ) : (
+        <>
+            <IconContext.Provider value={{color: "white", size: "2em", className: "open-menu-icon"}}>
+                <RxDropdownMenu onClick={toggleNavVisibility}/>
+            </IconContext.Provider>
+        </>
+        )}
     </ul>
+
  </nav>
+ 
   );
 }
 

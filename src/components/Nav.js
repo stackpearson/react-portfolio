6900ecbead22b9bcb7c7764598c1755136209ec4
Sawyer <sawyer.pearson92@gmail.com>
2023-12-06 13:48:48 -0700

    basic nav and routes complete

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,26 @@
 import '../styles/Nav.css';
+import {NavLink} from 'react-router-dom';
 
 function Nav() {
   return (
  <nav className='nav-main'>
-    <div className='nav-container'>
-        <div className='nav-item'>
-            <h2 className='nav-headers'>Home</h2>
-        </div>
-        <div className='nav-item'>
-            <h2 className='nav-headers'>Projects</h2>
-        </div>
-        <div className='nav-item'>
-            <h2 className='nav-headers'>Skills</h2>
-        </div>
-        <div className='nav-item'>
-            <h2 className='nav-headers'>About</h2>
-        </div>
-        <div className='nav-item'>
-            <h2 className='nav-headers'>Resume</h2>
-        </div>
-    </div>
+    <ul className='nav-container'>
+        <li className='nav-item'>
+            <NavLink to="/" className='nav-button'>Home</NavLink>
+        </li>
+        <li className='nav-item'>
+            <NavLink to="projects" className='nav-button'>Projects</NavLink>
+        </li>
+        <li className='nav-item'>
+            <NavLink to="skills" className='nav-button'>Skills</NavLink>
+        </li>
+        <li className='nav-item'>
+            <NavLink to="resume" className='nav-button'>Resume</NavLink>
+        </li>
+        <li className='nav-item'>
+            <NavLink to="contact" className='nav-button'>Contact</NavLink>
+        </li>
+    </ul>
  </nav>
   );
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,25 +1,24 @@
 import logo from '../img/logo.svg';
 import '../styles/App.css';
 import Nav from '../components/Nav.js';
+import {Route, Routes } from 'react-router-dom';
+import Home from './Home.js';
+import Projects from './Projects.js';
+import Resume from './Resume.js';
+import Skills from './Skills.js';
+import Contact from './Contact.js';
 
 function App() {
   return (<>
     <Nav />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='projects' element={<Projects/>}/>
+        <Route path='/skills' element={<Skills/>}/>
+        <Route path='/resume' element={<Resume/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   </>);
 }

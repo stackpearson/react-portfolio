7b76b7b9238e88e78d077b7032d83c82702fd9e5
Sawyer <sawyer.pearson92@gmail.com>
2023-12-06 23:54:28 -0700

    contact form created and hooked in

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import '../styles/Contact.css'
+import {useForm, ValidationError} from '@formspree/react';
 
 function Contact() {
+
+  const [state, handleSubmit] = useForm('mleyqjbb')
+  if (state.succeeded) {
+    return <div className='success-message'><div><p>Thanks for reaching out, I'll be in touch shortly!</p></div></div>;
+}
     return (
+      <>
       <div className='contact-container'>
-        <div>
-            <h1>Page under construction, a contact form will be here soon!</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className='contact-header'>
+              <h2>Let's get in touch!</h2>
+          </div>
+
+          <div className='contact-field'>
+            <div className='contact-input'>
+              <input id="email" type="email" name="email" placeholder='Your Email'/>
+            </div>
+            <ValidationError prefix="Email" field="email" errors={state.errors}/> 
+          </div>
+
+          <div className='contact-field'>
+            <div className='contact-input'>
+              <textarea id="message" name="message" placeholder='Please enter your message here'/>
+            </div>
+            <ValidationError prefix="Message" field="message" errors={state.errors}/>
+            <div>
+              <button type="submit" disabled={state.submitting}>Send</button>
+            </div>
+          </div>      
+        </form>
       </div>
+      </>
     );
 };
 
